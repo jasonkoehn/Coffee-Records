915dda06f6235650d3a23be704f817a3a79c9bf4
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-03-27 08:16:41 -0700

    Initial Commit

---
+++ Coffee Records/Coffee_RecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_RecordsApp.swift
+//  Coffee Records
+//
+//  Created by Jason Koehn on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_RecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
